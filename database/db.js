a231a225f7b1f1f831a42ea559346b3bd7051b6b
Mrinmoy Sarkar <mrin2ksarkar@gmail.com>
2022-03-04 20:22:27 +0530

    Misc

--- database/db.js
+++ database/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectToDB = async () => {
+  //console.log(process.env)
   const connect = await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
